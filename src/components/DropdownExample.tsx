ca037946b077bb69945f3b6f47c5c7553fec26d8
Ayush Patel <ayushp@palantir.com>
2025-11-22 15:39:53 -0500

    feat: continue iterating on v0 site, setup shadcn

---
+++ src/components/DropdownExample.tsx
@@ -0,0 +1,26 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function DropdownExample() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="px-4 py-2 bg-primary text-primary-foreground rounded-md hover:bg-primary/90">
+        Open Menu
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Billing</DropdownMenuItem>
+        <DropdownMenuItem>Team</DropdownMenuItem>
+        <DropdownMenuItem>Subscription</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
